b86e8d9221ef1ca451edb516ce371146cef2f487
crisog <ortega.cpp@gmail.com>
2024-11-26 14:14:26 -0400

    feat: adds viem

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Morpho Liquidator 🤖");
+import { createPublicClient, http } from "viem";
+import { mainnet } from "viem/chains";
+
+const client = createPublicClient({
+  chain: mainnet,
+  transport: http(
+    process.env.ETH_RPC_URL ?? "https://ethereum-public.nodies.app"
+  ),
+});
+
+(async () => {
+  console.log(
+    "Morpho Liquidator 🤖 started at block",
+    await client.getBlockNumber()
+  );
+})();
